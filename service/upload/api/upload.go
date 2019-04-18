aa8c6c36ca8715718343e70ddcbd4710fc72cde8
moguang <809444138@qq.com>
2019-04-18 13:44:11 +0800

    完善上传微服务

--- service/upload/api/upload.go
+++ service/upload/api/upload.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -136,4 +137,50 @@ func DoUploadHandler(c *gin.Context) {
 	} else {
 		errCode = -6
 	}
+}
+
+// TryFastUploadHandler : 尝试秒传接口
+func TryFastUploadHandler(c *gin.Context) {
+
+	// 1. 解析请求参数
+	username := c.Request.FormValue("username")
+	filehash := c.Request.FormValue("filehash")
+	filename := c.Request.FormValue("filename")
+	filesize, _ := strconv.Atoi(c.Request.FormValue("filesize"))
+
+	// 2. 从文件表中查询相同hash的文件记录
+	fileMeta, err := meta.GetFileMetaDB(filehash)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	// 3. 查不到记录则返回秒传失败
+	if fileMeta == nil {
+		resp := util.RespMsg{
+			Code: -1,
+			Msg:  "秒传失败，请访问普通上传接口",
+		}
+		c.Data(http.StatusOK, "application/json", resp.JSONBytes())
+		return
+	}
+
+	// 4. 上传过则将文件信息写入用户文件表， 返回成功
+	suc := dblayer.OnUserFileUploadFinished(
+		username, filehash, filename, int64(filesize))
+	if suc {
+		resp := util.RespMsg{
+			Code: 0,
+			Msg:  "秒传成功",
+		}
+		c.Data(http.StatusOK, "application/json", resp.JSONBytes())
+		return
+	}
+	resp := util.RespMsg{
+		Code: -2,
+		Msg:  "秒传失败，请稍后重试",
+	}
+	c.Data(http.StatusOK, "application/json", resp.JSONBytes())
+	return
 }
--- service/upload/route/router.go
+++ service/upload/route/router.go
@@ -3,6 +3,7 @@ package route
 import (
 	"filestore-server/service/upload/api"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,15 +18,26 @@ func Router() *gin.Engine {
 	// // 加入中间件，用于校验token的拦截器(将会从account微服务中验证)
 	// router.Use(handler.HTTPInterceptor())
 
+	// 使用gin插件支持跨域请求
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"}, // []string{"http://localhost:8080"},
+		AllowMethods:  []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Range", "x-requested-with", "content-Type"},
+		ExposeHeaders: []string{"Content-Length", "Accept-Ranges", "Content-Range", "Content-Disposition"},
+		// AllowCredentials: true,
+	}))
+
 	// Use之后的所有handler都会经过拦截器进行token校验
 
 	// 文件上传相关接口
 	router.POST("/file/upload", api.DoUploadHandler)
-	router.OPTIONS("/file/upload", func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		c.Status(204)
-	})
+	// 秒传接口
+	router.POST("/file/fastupload", api.TryFastUploadHandler)
+
+	// 分块上传接口
+	router.POST("/file/mpupload/init", api.InitialMultipartUploadHandler)
+	router.POST("/file/mpupload/uppart", api.UploadPartHandler)
+	router.POST("/file/mpupload/complete", api.CompleteUploadHandler)
 
 	return router
 }
